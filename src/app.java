a1ac2080bb670bf7d567727b975ef79d931fa5f4
Christian2019 <chrisschmidt2702@gmail.com>
2019-08-14 20:47:40 -0300

    v2

--- src/app.java
+++ src/app.java
@@ -1,19 +1,46 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class app {
 
 	public static void main(String[] args) throws FileNotFoundException {
-Leitura.lerTexto("arquivo.txt");	
+System.out.print("Por favor, digite o nome do arquivo de entrada: ");
+String nomearquivo ="";
+Scanner sc1 = new Scanner(System.in);
+nomearquivo=sc1.next();
+
+Leitura.lerTexto(nomearquivo+".txt");	
 
 Tarefas tarefa = new Tarefas();
 
-//tarefa.oac();
-//tarefa.oad();
-//tarefa.qco();
-tarefa.qdo();
-Gravar.salvar("arquivo");
+System.out.print("Opções de relatório:\r\n" + 
+		"(1) Ordem alfabética crescente das palavras\r\n" + 
+		"(2) Ordem alfabética decrescente das palavras\r\n" + 
+		"(3) Quantidade crescente de ocorrências\r\n" + 
+		"(4) Quantidade decrescente de ocorrências\r\n" + 
+		"Indique a opção desejada:");
+
+String numero ="";
+Scanner sc2 = new Scanner(System.in);
+numero=sc2.next();
+if (numero.equals("1")) {
+tarefa.oac();
+}
+if (numero.equals("2")) {
+	tarefa.oad();
+}
+if (numero.equals("3")) {
+	tarefa.qco();
+}
+if (numero.equals("4")) {
+	tarefa.qdo();
+}
+
+
+Gravar.salvar(nomearquivo);
+System.out.println("Relatório gerado. Arquivo "+nomearquivo+"-res.txt gerado.");
 		
 	}
 
